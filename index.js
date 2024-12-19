9217103d860c536d7baffb1adea5c536fdfa0ee4
Anyang Diing <anyang.ngor@student.moringaschool.com>
2024-12-19 09:18:13 +0300

    Complete Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Martin Scorsesi";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`; 
+// Export variables for testing
+module.exports = { currentUser, shortGreeting };
